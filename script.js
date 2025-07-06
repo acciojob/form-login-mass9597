18f58914750ab3c367984ee3a2513ee9a38bf241
Vicky@Developer <91041044+mass9597@users.noreply.github.com>
2025-07-06 16:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getFormvalue() {
     //Write your code here
 
+	const form = document.getElementById("form1");
+	const firstName = form.elements(`fname`).value().trim();
+	const lastName = form.elements(`lname`).value().trim();
+
+	if(!firstName && !lastName){
+		alert("please enter your firstName and lastName");
+		return;
+	}
+
+	const name = `${firstName} ${lastName}`.trim();
+	alert(name);
+	
+	
+
 }
+
